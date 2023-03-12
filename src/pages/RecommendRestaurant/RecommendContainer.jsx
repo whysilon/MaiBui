0681e342b23c1839e72f165d8ca0d77733061e44
Xavier Wong <91479624+WZHYX@users.noreply.github.com>
2023-03-12 20:17:22 +0800

    recommend restaurant structure

---
+++ src/pages/RecommendRestaurant/RecommendContainer.jsx
@@ -0,0 +1,14 @@
+import HomePageNavBar from "../../components/HomePageNavBar";
+import RecommendRestaurant from "./RecommendRestaurant";
+import "./RecommendContainer.css";
+
+function RecommendContainer() {
+  return (
+    <div>
+      <HomePageNavBar />
+      <RecommendRestaurant />
+    </div>
+  );
+}
+
+export default RecommendContainer;
---
+++ src/pages/RecommendRestaurant/RecommendRestaurant.jsx
@@ -0,0 +1,14 @@
+import "./RecommendRestaurant.css"
+import React from "react"
+
+function RecommendRestaurant()
+{
+
+    return(
+        <div className="recommend">
+            <h1 style={{fontSize: 40}}>Recommend Restaurants</h1>
+        </div>
+    )
+}
+
+export default RecommendRestaurant;
