7d2c669eef86357468fac09a86ebafe328045d59
Xavier Wong <91479624+WZHYX@users.noreply.github.com>
2023-03-14 17:04:14 +0800

    changes to search restaurant

--- src/pages/SearchRestaurant/SearchRestaurant.jsx
+++ src/pages/SearchRestaurant/SearchRestaurant.jsx
@@ -1,13 +1,23 @@
 import "./SearchRestaurant.css"
-import React from "react"
+import React, { useState } from "react"
 
 function SearchRestaurant()
 {
+    const [restaurantName, setRestaurantName] = useState("");
+
+    const searchHandler = (event) => {
+        setRestaurantName(event.target.value);
+      };
 
     return(
         <div className="search">
-            <h1 style={{fontSize: 40}}>Search for Restaurants</h1>
-            
+            <h1 style={{fontSize: 40}}>Search for Healthier Restaurants</h1>
+            <input
+            value={restaurantName}
+            onChange={searchHandler}
+            type="text"
+          />
+            <button type="submit">Search</button>
         </div>
     )
 }
