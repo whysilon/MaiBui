d0f552c94203430469f7dddaa58cd90eba83d9d2
Xavier Wong <91479624+WZHYX@users.noreply.github.com>
2023-03-14 17:04:01 +0800

    changes to recommend restaurants

--- src/pages/RecommendRestaurant/RecommendRestaurant.jsx
+++ src/pages/RecommendRestaurant/RecommendRestaurant.jsx
@@ -7,6 +7,7 @@ function RecommendRestaurant()
     return(
         <div className="recommend">
             <h1 style={{fontSize: 40}}>Recommend Restaurants</h1>
+            <button>Give me recommendations!</button>
         </div>
     )
 }
