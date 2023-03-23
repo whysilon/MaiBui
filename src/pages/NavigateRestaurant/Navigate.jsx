f2a53a40c2033f0a7b05635c2b7af3617c13e9ac
Xavier Wong <91479624+WZHYX@users.noreply.github.com>
2023-03-23 15:10:54 +0800

    modified location retrieval method

--- src/pages/NavigateRestaurant/Navigate.jsx
+++ src/pages/NavigateRestaurant/Navigate.jsx
@@ -1,36 +1,31 @@
 import "./Navigate.css";
+import GetLocation from "../../components/LocationRetriever";
 import React from 'react';
 
-const Navigate = () => {
-  const [latitude, setLatitude] = React.useState(0);
-  const [longitude, setLongitude] = React.useState(0);
+const Navigate = (props) => {
+  const {destlat, destlng} = props;
 
-  React.useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {  //Retrieve current location
-      const lat = position.coords.latitude, lng = position.coords.longitude;
-      setLatitude(lat);
-      setLongitude(lng)
-    })
-  }, [])
+  let location = GetLocation();
+
   const url = "https://www.google.com/maps/embed/v1/view?key="
-                + "-AIzaSyAJ1JqQHpz1BUe9HcakxwV8AhvQBR703ks"  //Remove "-" to enable map
-                + "&center=" + String(latitude) + ","
-                + String(longitude)
+                + "AIzaSyAJ1JqQHpz1BUe9HcakxwV8AhvQBR703ks"  //Remove "-" to enable map
+                + "&center=" + String(location.latitude) + ","
+                + String(location.longitude)
                 + "&zoom=15";
   return (
     <div className="navigation">
+      <h1>Restaurant Name</h1>
       <div className="block-1">
-        <h1>Restaurant Name</h1>
         <p>Current location: </p>
-        <p>{latitude}</p>
-        <p>{longitude}</p>
+        <p>{location.latitude}</p>
+        <p>{location.longitude}</p>
         <iframe title="Map"
             width="450"
             height="250"
             frameborder="0" style={{border:"0"}}
             referrerpolicy="no-referrer-when-downgrade"
             src={url}
-            allowfullscreen>
+            allowFullscreen>
         </iframe>
       </div>
     </div>
