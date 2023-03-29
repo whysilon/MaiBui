28f658cb0be4a5bd92f757fb6fe353cfa3e9ee31
Xing Mian <xlancer4@gmail.com>
2023-03-29 23:20:42 +0800

    application skeleton of account management

--- src/components/AccountCenterNavBar.jsx
+++ src/components/AccountCenterNavBar.jsx
@@ -1,6 +1,15 @@
+/**
+ * Displays the Navigation Bar, which includes the Side Bar, user avatar and link to account center.
+ * @see SideBar
+ * @see Backbutton
+ * @author Xing Mian
+ * @return HTML of Navigation Bar
+
+ */
+
 import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { Chip, Avatar } from "@mui/material";
+import { Avatar } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -8,18 +17,20 @@ import Typography from "@mui/material/Typography";
 import { AccountCircle, Logout } from "@mui/icons-material";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
-import { userProfileData } from "../pages/AccountCenterPage/userProfileData";
 import SideBar from "./SideBar";
 import IconButton from "@mui/material/IconButton";
 import Divider from "@mui/material/Divider";
 import Stack from "@mui/material/Stack";
+
+import { userProfileData } from "../pages/AccountCenterPage/userProfileData";
 import Backbutton from "../components/BackButton";
 
-//CSS
-// import "./NavBar.css";
-
-//Components
-
+/**
+ * This function is a component for the navigation bar in the account center page.
+ * @function AccountCenterNavBar
+ * @param {Object} props - Props for the component.
+ * @returns {JSX.Element} - Rendered component.
+ */
 function AccountCenterNavBar(props) {
   const location = useLocation();
   let navigate = useNavigate();
@@ -29,20 +40,38 @@ function AccountCenterNavBar(props) {
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
 
+  /**
+   * This function handles the opening of the user menu.
+   * @function handleMenu
+   * @param {Object} event - The event object.
+   */
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
+  /**
+   * This function handles the closing of the user menu.
+   * @function handleClose
+   */
   const handleClose = () => {
     setAnchorEl(null);
   };
 
+  /**
+   * This function handles the navigation to the account center page.
+   * @function handleAccountCenter
+   */
   const handleAccountCenter = () => {
     let path = `/account-center`;
     navigate(path);
   };
 
-  //function to get the current page name
+  /**
+   * This function capitalizes the last segment of the path.
+   * @function capitalizeLastSegment
+   * @param {string} str - The string to capitalize.
+   * @returns {string} - The capitalized string.
+   */
   function capitalizeLastSegment(str) {
     const segments = str.split("/");
     const lastSegment = segments[segments.length - 1];
@@ -55,8 +84,8 @@ function AccountCenterNavBar(props) {
   const currentLocation = capitalizeLastSegment(pathname);
 
   return (
-    //TODO:conditional redering of login/un logged in pages (home, account center and its child pages)
-    //TODO: databasae
+    //TODO:conditional redering based on authentication conditions
+    //TODO: GET user profile data from databasae
 
     <AppBar position="static" color="inherit" className="navbar">
       <Toolbar>
--- src/pages/ChangePasswordPage/ChangePasswordContainer.jsx
+++ src/pages/ChangePasswordPage/ChangePasswordContainer.jsx
@@ -1,3 +1,12 @@
+/**
+ * Displays the ChangePasswordContainer page, which is responsible for rendering the change password page.
+ * @see AccountCenterNavBar
+ * @see ChangePasswordForm
+ * @author Xing Mian
+ * @return HTML of Change Password Container
+ 
+ */
+
 import React from "react";
 import AccountCenterNavBar from "../../components/AccountCenterNavBar";
 import ChangePasswordForm from "./ChangePasswordForm";
