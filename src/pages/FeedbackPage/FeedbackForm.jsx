0fa8c34a94ce2ec2a9e701656c73945143b6b43f
Marcus Yeo <marcus.yks27@gmail.com>
2023-03-31 23:33:19 +0800

    Updated feedback

--- src/pages/FeedbackPage/FeedbackForm.jsx
+++ src/pages/FeedbackPage/FeedbackForm.jsx
@@ -1,6 +1,7 @@
 import "./FeedbackForm.css";
 import React, { useState, useEffect} from "react";
 import { TextField } from "@mui/material";
+import StarRating from "../../components/StarRating";
 
 /**
  * Displays the feedback form showed on
@@ -11,37 +12,20 @@ import { TextField } from "@mui/material";
  */
 
 const FeedbackForm = () => {
-
+  /**
+   * Sets rating storage based on change
+   * in StarRating componenet
+   * 
+   * @param {onClick} event 
+   */
+  const saveStarRatingHandler = (enteredStarRating) => {
+    setEnteredRating(enteredStarRating)
+  }
   /**Storage/setters for feedback form 
    * input variables */
-
-  const [enteredName, setEnteredName] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
+  const [enteredRating, setEnteredRating] = useState(0);
   const [enteredExp, setEnteredExp] = useState("");
   const [expCount, setExpCount] = useState("");
-
-  /**
-   * Sets name storage based on change
-   * in name input
-   * 
-   * @param {onChange} event 
-   */
-
-  const nameHandler = (event) => {
-    setEnteredName(event.target.value);
-
-  };
-
-  /**
-   * Sets age storage based on change
-   * in age input
-   * 
-   * @param {onChange} event 
-   */
-
-  const ageHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
 
   /**
    * Sets experience storage based on change
@@ -74,19 +58,17 @@ const FeedbackForm = () => {
     event.preventDefault();
 
     let feedbackDetails = {
-      name: enteredName,
-      age: enteredAge,
+      rating: enteredRating,
       exp: enteredExp,
     };
 
     //Checks if details are blank
     if (
-      feedbackDetails.name === "" ||
-      feedbackDetails.age === "" ||
+      feedbackDetails.rating === 0 ||
       feedbackDetails.exp === ""
     ) {
       alert("Leave no fields blank!");
-      feedbackDetails = {name:"",age:"",exp:""};
+      feedbackDetails = {rating:0,exp:""};
       return;
     }
 
@@ -96,26 +78,18 @@ const FeedbackForm = () => {
       return;
     }
 
-    //Checks if name is too long
-    if(feedbackDetails.name.length > 512){
-      alert("Invalid name!");
-      return;
-    }
     //else checks details with database
     else {
       console.log(feedbackDetails);
       alert("Successful submission!");
-      setEnteredName("");
-      setEnteredAge("");
+      setEnteredRating(0)
       setEnteredExp("");
       setExpCount(0);
     }
   };
-
   /**
    * Updates character count of experience input
    */
-
   useEffect(() => {
     // update char count (including whitespaces)
     setExpCount(enteredExp.length);
@@ -126,47 +100,15 @@ const FeedbackForm = () => {
       <div className="feedbackblock-1">
         <div className="details">
           <h1>Feedback</h1>
-          <p>Your feedback is greatly appreciated.</p>
+          <span>Your feedback is greatly appreciated.</span>
         </div>
       </div>
+
       <div className="feedbackblock-2">
-        <div className="NameAndAge">
-          <p>Name:</p>
-          <TextField
-                value={enteredName}
-                onChange={nameHandler}
-                type={"text"}
-                variant="outlined"
-                label="Enter your name"
-                margin="normal"
-                helperText={
-                  enteredName === ""
-                    ? "Empty field!"
-                    : // : enteredNewPwd !== enteredConfirmedPwd
-                      // ? "Passwords do not match!"
-                      ""
-                }
-              ></TextField>
-          <p>Age:</p>
-          <TextField
-                className="age"
-                value={enteredAge}
-                onChange={ageHandler}
-                type={"number"}
-                variant="outlined"
-                margin="normal"
-                InputProps={{ inputProps: { min: 18, max: 150 } }}
-                helperText={
-                  enteredAge === ""
-                    ? "Empty field!"
-                    : // : enteredNewPwd !== enteredConfirmedPwd
-                      // ? "Passwords do not match!"
-                      ""
-                }
-              ></TextField>
+        <div className="ratingName">
+          <p>Rating:</p>
+          <StarRating onSaveStarRating = {saveStarRatingHandler} />
         </div>
-      </div>
-      <div className="feedbackblock-3">
         <div className = "experience">
           <p>Details Of Experience:</p>
           <TextField
@@ -188,7 +130,7 @@ const FeedbackForm = () => {
                 }
               ></TextField>
         </div>
-        <div>
+        <div className = "feedbackBottom">
           <span id="exp">Character Count: {expCount}</span>
           <button type="submit">Submit</button>
         </div>
